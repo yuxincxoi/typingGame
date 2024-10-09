5ab6a1f8d80e0d480d67b70e5f8051a627f2323f
최유진 <yjyj0774@gmail.com>
2024-10-09 22:20:24 +0900

    정적 요소로 가져오기

--- src/app.js
+++ src/app.js
@@ -1,69 +1,43 @@
-import { initTime, timerp } from "../modules/initTime.js";
-import { loosingHeart, chance, chancep } from "../modules/loosingHeart.js";
-import { randomExample, example } from "../modules/randomExample.js";
-import { clear, input, score, scorep } from "../modules/clear.js";
+import { initTime, timer } from "../modules/initTime.js";
+import { loosingHeart } from "../modules/loosingHeart.js";
+import { randomExample } from "../modules/randomExample.js";
+import { clear, score } from "../modules/clear.js";
+
+const form = document.querySelector("form");
+const typingInput = document.getElementById("typing-input");
+const typingSentence = document.getElementById("typing-sentence");
+const totalScore = document.getElementById("total-score");
+const submitBtn = document.getElementById("submit-button");
 
 const root = document.getElementById("root");
-
-// 받아쓰기 문장
-const expBox = document.createElement("div");
-
-// 입력 form
-const inputBox = document.createElement("div");
-const form = document.createElement("form");
-const btn = document.createElement("input");
-const reset = document.createElement("input");
-const startBtn = document.createElement("input");
-
-btn.type = "button";
-btn.value = "Enter";
-reset.type = "button";
-reset.value = "Reset";
+const startBtn = document.createElement("button");
 startBtn.type = "button";
 startBtn.value = "Start";
+
+root.append(startBtn, timer);
 
 // 추가된 스타트 버튼
 inputBox.appendChild(startBtn);
 
 randomExample();
 
-// 점수
-const scoreBox = document.createElement("div");
-
-scoreBox.appendChild(scorep);
-
 localStorage.setItem("score", 0);
 scorep.innerText = `점수 : ${localStorage.getItem("score")}`;
-
-// 생명
-const chanceBox = document.createElement("div");
-
-chanceBox.appendChild(chancep);
 
 localStorage.setItem("chance", chance);
 chancep.innerText = `Chance : ${localStorage.getItem("chance")}`;
 
-// 타이머
-const timerBox = document.createElement("div");
-timerBox.appendChild(timerp);
-
-// element create
-expBox.appendChild(example);
-root.append(expBox, inputBox, scoreBox, chanceBox, timerBox);
-inputBox.appendChild(form);
-form.append(input, btn, reset);
-
 // Enter를 눌렀을 때
 form.addEventListener("submit", (e) => {
-  if (input.value === example.innerText) {
+  if (typingInput.value === typingSentence.innerText) {
     // 입력 값이 문장과 같으면
     score += 5;
     localStorage.setItem("score", score);
-    scorep.innerText = `점수 : ${localStorage.getItem("score")}`;
+    totalScore.innerText = `점수 : ${localStorage.getItem("score")}`;
     // 다음 문장 할당
     randomExample();
     // 입력창 초기화
-    input.value = "";
+    typingInput.value = "";
     // 타이머 초기화
     initTime();
   } else {
