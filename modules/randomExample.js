8d2ffd9c94deabc2305726cc84f3151889a149b7
최유진 <yjyj0774@gmail.com>
2024-10-09 22:08:40 +0900

    random data 반환

--- modules/randomExample.js
+++ modules/randomExample.js
@@ -2,7 +2,7 @@ import data from "../data.js";
 
 const randomExample = () => {
   const randomData = data[Math.floor(Math.random() * data.length)];
-  example.innerText = randomData;
+  return randomData;
 };
 
 export { randomExample };
