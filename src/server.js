bf4dbedf2f82f9d09c49be3d53852ac8f4389ed1
최유진 <yjyj0774@gmail.com>
2024-10-11 18:06:28 +0900

    임시 server

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// 정적 파일 제공
+app.use(express.static(path.join(__dirname, "..")));
+
+// 홈 페이지
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../index.html"));
+});
+
+// About 페이지
+app.get("/main", (req, res) => {
+  res.sendFile(path.join(__dirname, "../main.html"));
+});
+
+// Contact 페이지
+app.get("/over", (req, res) => {
+  res.sendFile(path.join(__dirname, "../over.html"));
+});
+
+// 서버 실행
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
