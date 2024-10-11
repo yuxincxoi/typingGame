0e2cb1c095d13a224885bc8adc22f63b57c6851c
최유진 <yjyj0774@gmail.com>
2024-10-11 19:45:32 +0900

    랜덤 데이터로 만든 랜덤 배열 array return

--- modules/randomExample.js
+++ modules/randomExample.js
@@ -1,8 +1,12 @@
 import data from "../data.js";
 
+let array = [];
+
 const randomExample = () => {
   const randomData = data[Math.floor(Math.random() * data.length)];
-  return randomData;
+  array.push(randomData);
+
+  return array;
 };
 
 export { randomExample };
