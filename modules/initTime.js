40d784f41b16b9ced54fd3c9b4606be1b7104eb3
최유진 <yjyj0774@gmail.com>
2024-10-13 18:31:51 +0900

    변수명 및 timer 시간 변경

--- modules/initTime.js
+++ modules/initTime.js
@@ -2,25 +2,24 @@ import { loosingHeart } from "./loosingHeart.js";
 import updateTimerLine from "./updateTimerLine.js";
 
 let timerInterval = null; // 타이머 인터벌을 저장할 변수
-let i = 10;
+let count = 15;
 
 const initTime = (isGameOver = false) => {
   clearInterval(timerInterval);
 
   if (isGameOver) {
-    i = 0;
-    updateTimerLine(i);
+    count = 0;
+    updateTimerLine(count);
     return; // Game Over 상태에서는 타이머를 더 이상 실행하지 않음
   }
 
-  i = 15;
-  updateTimerLine(i);
+  updateTimerLine(count);
 
   timerInterval = setInterval(() => {
-    i -= 1;
-    updateTimerLine(i);
+    count -= 1;
+    updateTimerLine(count);
 
-    if (i <= 0) {
+    if (count <= 0) {
       clearInterval(timerInterval);
       loosingHeart();
     }
