f6a73d9c14fb4788eabdccda1571ad6980d9f021
최유진 <yjyj0774@gmail.com>
2024-10-13 18:54:30 +0900

    example 갯수 변경

--- modules/randomArray.js
+++ modules/randomArray.js
@@ -2,7 +2,7 @@ import data from "../data.js";
 
 const randomArray = () => {
   let basicArray = [];
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < 25; i++) {
     const randomData = data[Math.floor(Math.random() * data.length)];
     basicArray.push(randomData);
   }
